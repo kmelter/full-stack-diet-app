acd91bd0db05d0ba8013319760034337ac1074c0
kmelter <kmelter97@gmail.com>
2022-05-11 20:49:09 -0500

    successful get all foods

--- routes/api/food-routes.js
+++ routes/api/food-routes.js
@@ -3,6 +3,16 @@ const router = require('express').Router();
 const { Food } = require('../../models');
 
 // GET all foods
+router.get('/', (req, res) => {
+    Food.findAll({})
+    .then((foodData) => {
+        res.json(foodData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // GET one food
 
@@ -11,3 +21,5 @@ const { Food } = require('../../models');
 // GET food by diet(s)
 
 // Update one food
+
+module.exports = router;
---
+++ routes/api/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const foodRoutes = require('./food-routes');
+
+router.use('/food', foodRoutes);
+
+module.exports = router;
