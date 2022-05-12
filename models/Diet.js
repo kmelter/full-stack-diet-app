45789d30e6a2123d8f27bf7193a9231662331cde
kmelter <kmelter97@gmail.com>
2022-05-11 19:56:40 -0500

    main merge

--- models/Diet.js
+++ models/Diet.js
@@ -2,3 +2,19 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
+class Diet extends Mode {}
+
+Diet.init(
+    {
+        //define columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        diet_name: {
+            
+        }
+    }
+)
