1550f6b552a9181d2107c95219089da735fdb2db
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-05-11 20:29:47 -0500

    commented some desired food routes

--- routes/api/food-routes.js
+++ routes/api/food-routes.js
@@ -2,3 +2,12 @@ const router = require('express').Router();
 
 const { Food } = require('../../models');
 
+// GET all foods
+
+// GET one food
+
+// Create one food
+
+// GET food by diet(s)
+
+// Update one food
