c35f3f3339203b0250cfad1842cc2e0aff3106dd
Nibbler <kwimbish@hotmail.com>
2022-05-16 20:28:56 -0500

    home-routes update

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,4 +4,18 @@ const { Diet, Food, FoodDiet } = require('../models');
 const withAuth = require('../utils/auth');
 
 // log in screen/ welcome "I can has Cheeseburger? Log in to find out."
+// router.get('/', withAuth, async (req, res) => {
+//     try {
+      
+//     }
 
+// router.get('/login', (req, res) => {
+//     if (req.session.loggedIn) {
+//       res.redirect('/');
+//       return;
+//     }
+  
+//     res.render('login');
+//   });
+  
+//   module.exports = router;
